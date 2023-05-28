51479583a5c6e6468c1577276a4d7bff67e76cbe
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2023-05-28 13:16:41 +0300

    ЛР№6

---
+++ OOPLab6T/6.2/6.2/6.2.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+using namespace std;
+
+class Quadrilateral {
+public:
+    virtual double area() = 0;
+    virtual double perimeter() = 0;
+};
+
+class Square : public Quadrilateral {
+private:
+    double side;
+public:
+    Square(double s) {
+        side = s;
+    }
+    double area() {
+        return side * side;
+    }
+    double perimeter() {
+        return 4 * side;
+    }
+};
+
+class Rectangle : public Quadrilateral {
+private:
+    double length, width;
+public:
+    Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
+    double area() {
+        return length * width;
+    }
+    double perimeter() {
+        return 2 * (length + width);
+    }
+};
+
+class Parallelogram : public Quadrilateral {
+private:
+    double base, height, side;
+public:
+    Parallelogram(double b, double h, double s) {
+        base = b;
+        height = h;
+        side = s;
+    }
+    double area() {
+        return base * height;
+    }
+    double perimeter() {
+        return 2 * (base + side);
+    }
+};
+
+class Trapezoid : public Quadrilateral {
+private:
+    double base1, base2, height, side1, side2;
+public:
+    Trapezoid(double b1, double b2, double h, double s1, double s2) {
+        base1 = b1;
+        base2 = b2;
+        height = h;
+        side1 = s1;
+        side2 = s2;
+    }
+    double area() {
+        return 0.5 * (base1 + base2) * height;
+    }
+    double perimeter() {
+        return base1 + base2 + side1 + side2;
+    }
+};
+
+int main() {
+    Square sq(5);
+    cout << "Area of square: " << sq.area() << endl;
+    cout << "Perimeter of square: " << sq.perimeter() << endl;
+
+    Rectangle rect(4, 6);
+    cout << "Area of rectangle: " << rect.area() << endl;
+    cout << "Perimeter of rectangle: " << rect.perimeter() << endl;
+
+    Parallelogram para(5, 8, 6);
+    cout << "Area of parallelogram: " << para.area() << endl;
+    cout << "Perimeter of parallelogram: " << para.perimeter() << endl;
+
+    Trapezoid trap(5, 8, 6, 7, 9);
+    cout << "Area of trapezoid: " << trap.area() << endl;
+    cout << "Perimeter of trapezoid: " << trap.perimeter() << endl;
+
+    return 0;
+}
---
+++ OOPLab6T/6.3/6.3/6.3.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+protected:
+    string name;
+    int age;
+public:
+    Person(string n, int a) {
+        name = n;
+        age = a;
+    }
+    virtual ~Person() {}
+    virtual void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+    friend istream& operator>>(istream& in, Person& p) {
+        cout << "Enter name: ";
+        in >> p.name;
+        cout << "Enter age: ";
+        in >> p.age;
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const Person& p) {
+        out << "Name: " << p.name << endl;
+        out << "Age: " << p.age << endl;
+        return out;
+    }
+};
+
+class Father : public virtual Person {
+protected:
+    int income;
+public:
+    Father(string n, int a, int i) : Person(n, a) {
+        income = i;
+    }
+    virtual ~Father() {}
+    virtual void display() {
+        Person::display();
+        cout << "Income: " << income << endl;
+    }
+    friend istream& operator>>(istream& in, Father& f) {
+        Person& p = f;
+        in >> p;
+        cout << "Enter income: ";
+        in >> f.income;
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const Father& f) {
+        const Person& p = f;
+        out << p;
+        out << "Income: " << f.income << endl;
+        return out;
+    }
+};
+
+class Mother : public virtual Person {
+protected:
+    int education;
+public:
+    Mother(string n, int a, int e) : Person(n, a) {
+        education = e;
+    }
+    virtual ~Mother() {}
+    virtual void display() {
+        Person::display();
+        cout << "Education level: " << education << endl;
+    }
+    friend istream& operator>>(istream& in, Mother& m) {
+        Person& p = m;
+        in >> p;
+        cout << "Enter education level: ";
+        in >> m.education;
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const Mother& m) {
+        const Person& p = m;
+        out << p;
+        out << "Education level: " << m.education << endl;
+        return out;
+    }
+};
+
+class Son : public Father, public Mother {
+protected:
+    string hobby;
+public:
+    Son(string n, int a, int i, int e, string h) : Person(n, a), Father(n, a, i), Mother(n, a, e) {
+        hobby = h;
+    }
+    virtual ~Son() {}
+    virtual void display() {
+        Father::display();
+        Mother::display();
+        cout << "Hobby: " << hobby << endl;
+    }
+    friend istream& operator>>(istream& in, Son& s) {
+        Father& f = s;
+        Mother& m = s;
+        in >> f;
+        in >> m;
+        cout << "Enter hobby: ";
+        in >> s.hobby;
+        return in;
+    }
+    friend ostream& operator<<(ostream& out, const Son& s) {
+        const Father& f = s;
+        const Mother& m = s;
+        out << f;
+        out << m;
+        out << "Hobby: " << s.hobby << endl;
+        return out;
+    }
+};
+
+int main() {
+    Son s("John", 20, 50000, 4, "Playing football");
+    cout << "Son's information:" << endl;
+    s.display();
+    cout << endl;
+
+    Son s2("Mary", 18, 45000, 5, "Playing guitar");
+    cout << "Enter information for second son:" << endl;
+    cin >> s2;
+    cout << "Second son's information:" << endl;
+    cout << s2 << endl;
+
+    return 0;
+}
